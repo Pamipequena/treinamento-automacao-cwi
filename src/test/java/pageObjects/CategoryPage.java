b049e91711c9a541208338b94aef796f65082c1a
Pamela Carvalho <pamscarvalho@hotmail.com>
2020-09-19 01:40:44 -0300

    implementando_testes

---
+++ src/test/java/pageObjects/CategoryPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import elementMapper.CategoryPageElementMapper;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class CategoryPage extends CategoryPageElementMapper {
+
+    public CategoryPage(){
+        PageFactory.initElements(Browser.getCurrentDriver(), this);
+    }
+
+    public boolean isPageTshirts(){
+        return getAuthenticationPageTShirts().contains("T-SHIRTS");
+    }
+
+    public String getAuthenticationPageTShirts(){
+        return nameCategoryTShirts.getText();
+    }
+
+    public void clickProductAddToProductPage(){
+        BasePage.mouserOver(productNameCategory);
+        buttonMoreAddToProductPage.click();
+    }
+
+    public String getProductNameCategory(){
+        return productNameCategory.getText();
+    }
+}
--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,14 +1,16 @@
 package tests;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObjects.*;
 import utils.Browser;
 import utils.Utils;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class SetupTest extends BaseTests{
 
@@ -19,7 +21,7 @@ public class SetupTest extends BaseTests{
     }
 
     @Test
-    public void testLogin(){
+    public void testLogin() {
         Browser.getCurrentDriver().findElement(By.className("login")).click();
         System.out.println("Clicou em Sing In e direcionou para a página de login");
         assertTrue(Browser.getCurrentDriver().getCurrentUrl()
@@ -36,5 +38,84 @@ public class SetupTest extends BaseTests{
         System.out.println("Validou Minha conta no Site");
     }
 
-    
+    @Test
+    public void testeSearch(){
+
+        String quest = "DRESS";
+        String questResultQtd = "7";
+
+        // Iniciar as páginas
+        HomePage home = new HomePage();
+        SearchPage search = new SearchPage();
+
+        //Fazer a pesquisa
+        home.doSearch(quest);
+
+        //Validar a pesquisa
+        assertTrue(search.isSearchPage());
+        assertEquals(search.getTextLighter().replace("\"", ""),quest);
+        assertThat(search.getTextHeading_counter(), CoreMatchers.containsString(questResultQtd));
+
+    }
+
+    @Test
+    public void testAcessCategoryTShirts(){
+
+        //Iniciar as páginas
+        HomePage home = new HomePage();
+        CategoryPage category = new CategoryPage();
+
+        //Clicar na categoria T-Shirts
+        home.clickCategoryTShirts();
+        //Browser.getCurrentDriver().findElement(By.linkText("T-SHIRTS")).click();
+
+        //Validar se ao clicar na categoria T_SHIRTS ocorre o direcionamento correto
+        assertTrue(category.isPageTshirts());
+    }
+
+    @Test
+    public void testAddProductToProductPage(){
+
+        //Acessar a categoria T-Shirts
+        testAcessCategoryTShirts();
+
+        //Iniciar as páginas
+        CategoryPage category = new CategoryPage();
+        ProductPage pdp = new ProductPage();
+
+        //Salva o nome do produto na página de categoria
+        String nameProductCategory = category.getProductNameCategory();
+
+        //Clicaar em More e direcionar para a página do produto
+        category.clickProductAddToProductPage();
+
+        //Verifica se produto está na página de detalhess do produto corretamente
+        assertTrue(pdp.getProductNamePDP().equals(nameProductCategory));
+
+    }
+
+    @Test
+    public void testAddProductToCartPage(){
+        //Acessa a página de produto
+        testAddProductToProductPage();
+
+        //Iniciar as páginas
+        ProductPage pdp = new ProductPage();
+        CartPage cart = new CartPage();
+
+        //Salvar o nome do produto na página PDP
+        String nameProductPDP = pdp.getProductNamePDP();
+
+        //Clicar no botão de Adicionar ao carrinho
+        pdp.clickButtonAddToCart();
+
+        //Clicar no botão Proceed To Checkout da modal
+        pdp.clickButtonModalProceedToCheckout();
+
+        //Validação do nome do produto no carrinho
+        assertTrue(cart.getNameProductCart().equals(nameProductPDP));
+
+
+
+    }
 }
